9b4ba9ae8cbc7f6a5dd73da9c73f401ed10998f5
victordavanco <61791150+victordavanco@users.noreply.github.com>
2020-11-03 23:54:50 -0300

    server

---
+++ server/README.md
@@ -0,0 +1,12 @@
+# Funcionalidades
+
+## Conexões
+
+- Rota para listar total de conexões realizadas;
+- Rota para criar uma nova conexão;
+
+## Rotas
+
+- Rota para criar uma aula;
+- Rota de listar aulas;
+    - Filtrar por materia, dia da semana e horário;
---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+//GET:buscar ou listar informação
+//POST:criar informação
+//PUT:atualizar informação
+//DELETE:deletar informação
+
+//Corpo (Request Budy)
+//Route Params:Identificar qual requrso eu quero atualizar ou deletar
+//Query Params: Paginação, filtros, ordenação
+
+
+
+//localhost:3333
+
+app.listen(3333);
